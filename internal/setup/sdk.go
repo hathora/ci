378aad899b94f885c3b1f60981c5fedd8a3a20c8
Matthew Saxon <matthew@geode.io>
2024-05-15 09:09:12 -0500

    pr feedback

--- internal/setup/sdk.go
+++ internal/setup/sdk.go
@@ -8,8 +8,6 @@ import (
 func SDK(token string, url string) *sdk.SDK {
 	sdkInstance := sdk.New(
 		sdk.WithSecurity(shared.Security{ HathoraDevToken: sdk.String(token)}),
-		sdk.WithAppID("app-af469a92-5b45-4565-b3c4-b79878de67d2"),
 		sdk.WithServerURL(url))
-	
 	return sdkInstance
 }
