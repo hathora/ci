79914c652299ff5e01bb3d30b64794ff830b1523
Matthew Saxon <matthew@geode.io>
2024-05-22 15:20:09 -0500

    adding logic to respect gitignore and dockerignore files

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
--- internal/archive/targz.go
+++ internal/archive/targz.go
@@ -3,6 +3,7 @@ package archive
 import (
 	"archive/tar"
 	"compress/gzip"
+	"github.com/monochromegane/go-gitignore"
 	"io"
 	"net/http"
 	"os"
@@ -17,9 +18,35 @@ type TGZFile struct {
 	Path    string
 }
 
+func getIgnoreMatchers(srcFolder string, filepaths ...string) ([]gitignore.IgnoreMatcher, error) {
+	var matchers []gitignore.IgnoreMatcher
+	for _, path := range filepaths {
+		matcher, err := gitignore.NewGitIgnore(filepath.Join(srcFolder, path), ".")
+		if err != nil {
+			return nil, err
+		}
+
+		matchers = append(matchers, matcher)
+	}
+
+	return matchers, nil
+}
+
+func shouldIgnoreFilepath(filepath string, isDir bool, matchers []gitignore.IgnoreMatcher) bool {
+	anyMatches := false
+	for _, matcher := range matchers {
+		if matcher.Match(filepath, isDir) {
+			anyMatches = true
+			break
+		}
+	}
+
+	return anyMatches
+}
+
 func ArchiveTGZ(srcFolder string) (*TGZFile, error) {
-	destFile := filepath.Clean(srcFolder) + ".tgz"
-	tarGzFile, err := os.Create(destFile)
+	destinationFile := filepath.Clean(srcFolder) + ".tgz"
+	tarGzFile, err := os.Create(destinationFile)
 	if err != nil {
 		return nil, err
 	}
@@ -30,6 +57,11 @@ func ArchiveTGZ(srcFolder string) (*TGZFile, error) {
 	tarWriter := tar.NewWriter(gzipWriter)
 	defer tarWriter.Close()
 
+	ignoreMatchers, err := getIgnoreMatchers(srcFolder, filepath.Join(srcFolder, ".dockerignore"), filepath.Join(srcFolder, ".gitignore"))
+	if err != nil {
+		return nil, err
+	}
+
 	err = filepath.Walk(srcFolder, func(filePath string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -38,6 +70,10 @@ func ArchiveTGZ(srcFolder string) (*TGZFile, error) {
 		relPath, err := filepath.Rel(srcFolder, filePath)
 		if err != nil {
 			return err
+		}
+
+		if shouldIgnoreFilepath(relPath, info.IsDir(), ignoreMatchers) {
+			return nil
 		}
 
 		header, err := tar.FileInfoHeader(info, relPath)
@@ -68,15 +104,15 @@ func ArchiveTGZ(srcFolder string) (*TGZFile, error) {
 		return nil
 	})
 
-	content, err := os.ReadFile(destFile)
+	content, err := os.ReadFile(destinationFile)
 	if err != nil {
 		return nil, err
 	}
 
 	file := &TGZFile{
 		Content: content,
-		Name:    filepath.Base(destFile),
-		Path:    destFile,
+		Name:    filepath.Base(destinationFile),
+		Path:    destinationFile,
 	}
 
 	return file, nil
