bfbff7036002f7bb4e39b7d9df760f16d55ceed4
Zach Langbert <zach@geode.io>
2024-06-30 13:51:19 -0700

    improve powershell install script

--- install.ps1
+++ install.ps1
@@ -1,9 +1,10 @@
+# Enable strict mode and exit on error
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
 $repoOwner = "hathora"
 $repoName = "ci"
 $binaryName = "hathora"
-
-# Get the latest release tag from GitHub
-$latestRelease = (Invoke-WebRequest "https://api.github.com/repos/$repoOwner/$repoName/releases/latest" | ConvertFrom-Json).tag_name
 
 # Detect the architecture
 $arch = $env:PROCESSOR_ARCHITECTURE
@@ -17,17 +18,13 @@ if ($arch -eq "AMD64") {
 }
 
 # Set the download URL for the Windows binary based on the architecture
-$downloadUrl = "https://github.com/$repoOwner/$repoName/releases/download/$latestRelease/$binaryName-windows-$arch"
+$downloadUrl = "https://github.com/$repoOwner/$repoName/releases/latest/download/$binaryName-windows-$arch.exe"
 
 # Set the install directory
 $installDir = "$env:LOCALAPPDATA\Microsoft\WindowsApps"
 
-# Download the binary
-Write-Host "Downloading $binaryName for $arch..."
-Invoke-WebRequest -Uri $downloadUrl -OutFile "$binaryName.exe"
-
-# Install the binary
-Write-Host "Installing $binaryName to $installDir..."
-Move-Item -Path "$binaryName.exe" -Destination $installDir -Force
+# Download and install the binary directly to the destination
+Write-Host "Downloading and installing $binaryName for $arch to $installDir..."
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$installDir\$binaryName.exe"
 
 Write-Host "Installation complete!"
