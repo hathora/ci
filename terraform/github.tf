6af7315f3fd66b9133cbb7e1cb73cce1aadfb458
Ryan Zander <ryan@geode.io>
2024-10-03 16:44:21 -0700

    Removed the certificate renewal artifacts.

--- terraform/github.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "certificate_pfx" {
-  repository       = "hathora/ci"
-  secret_name      = "CERTIFICATE_PFX"
-  encrypted_value  = acme_certificate.certificate.certificate_p12
-}
